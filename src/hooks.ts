a50cc1146913aae07d4b7d99398e532da0f5e40e
Manuel Valls <me@manvalls.com>
2025-01-03 15:20:40 +0100

    Simplify useFormState hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,9 +3,9 @@ import { useContext } from "react";
 import type { FormState } from "./types";
 import { FormStateContext } from "./context";
 
-export const useFormState = <ResultType = any, ErrorType = any>() => {
-  const formState: FormState<ResultType, ErrorType> =
-    useContext(FormStateContext);
-
-  return formState;
+export const useFormState = <ResultType = any, ErrorType = any>(): FormState<
+  ResultType,
+  ErrorType
+> => {
+  return useContext(FormStateContext);
 };
